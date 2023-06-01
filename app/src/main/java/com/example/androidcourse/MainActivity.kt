5bddbf240c41d75649f69f0d9f02f19e3119e17d
DIALLO <ousmane.diallo1.sio@gmail.com>
2023-06-01 12:57:21 +0200

    feat: splashscreen and constraint layouts

--- app/src/main/java/com/example/androidcourse/MainActivity.kt
+++ app/src/main/java/com/example/androidcourse/MainActivity.kt
@@ -1,11 +1,76 @@
 package com.example.androidcourse
 
+import android.annotation.SuppressLint
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.StyleSpan
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var ivOverview: ImageView
+    private lateinit var tvBarcode: TextView
+    private lateinit var tvAmount: TextView
+    private lateinit var tvCountryOfSale: TextView
+    private lateinit var tvIngredients: TextView
+    private lateinit var tvAllergens: TextView
+    private lateinit var tvAdditives: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.product_sheet)
+
+        ivOverview = findViewById(R.id.ivOverviewProductSheet)
+        tvBarcode = findViewById(R.id.tvBarcodeProductSheet)
+        tvAmount = findViewById(R.id.tvAmountProductSheet)
+        tvCountryOfSale = findViewById(R.id.tvCountryOfSellProductSheet)
+        tvIngredients = findViewById(R.id.tvIngredientsProductSheet)
+        tvAllergens = findViewById(R.id.tvAllergensProductSheet)
+        tvAdditives = findViewById(R.id.tvAdditivesProductSheet)
+
+        supportActionBar?.setBackgroundDrawable(
+            ContextCompat.getDrawable(this, R.drawable.main_gradient)
+        )
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onStart() {
+        super.onStart()
+
+        Glide.with(this)
+            .load("https://i.stack.imgur.com/WWTA9.png")
+            .into(ivOverview)
+
+        val tvs = mapOf(
+            tvBarcode to "1234567890123",
+            tvAmount to "400 g (280 g net égoutté)",
+            tvCountryOfSale to "France, Japon, Suisse",
+            tvIngredients to "Pommes de terre, huile de tournesol, sel, petits pois, carottes, oignons, poireaux, persil, poivre.",
+            tvAllergens to "Aucun",
+            tvAdditives to "Aucun"
+        )
+
+        tvs.keys.forEach {
+            it.text = "${it.text} ${tvs[it]}"
+            it.applyBoldText(":")
+        }
+    }
+
+    private fun TextView.applyBoldText(limit: String) {
+        val spannableString = SpannableString(this.text)
+        spannableString.setSpan(
+            StyleSpan(Typeface.BOLD),
+            0,
+            this.text.indexOf(limit),
+            0
+        )
+        this.text = spannableString
     }
 }
