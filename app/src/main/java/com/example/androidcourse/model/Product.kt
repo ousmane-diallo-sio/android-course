9b685a82d71dda7be22b2429da2728886cb64ef6
DIALLO <ousmane.diallo1.sio@gmail.com>
2023-06-26 15:46:31 +0200

    refactor: use guiline virtual view to define margin

---
+++ app/src/main/java/com/example/androidcourse/model/Product.kt
@@ -0,0 +1,38 @@
+package com.example.androidcourse.model
+
+data class Product(
+    val name: String,
+    val brand: String,
+    val nutriScore: NutriScore,
+    val barcode: String,
+    val thumbnail: String,
+    val quantity: String,
+    val countries: List<String>,
+    val ingredients: List<String>,
+    val allergens: List<String>,
+    val additives: List<String>,
+)
+
+enum class NutriScore(val label: String) {
+    A("A"), B("B"), C("C"), D("D"), E("E"), Unknown("")
+}
+
+fun generateFakeProduct() = Product(
+    name = "Petits pois et carottes",
+    brand = "Cassegrain",
+    nutriScore = NutriScore.A,
+    barcode = "3083680085304",
+    thumbnail = "https://static.openfoodfacts.org/images/products/308/368/008/5304/front_fr.7.400.jpg",
+    quantity = "400g",
+    countries = listOf("France", "Japon", "Suisse"),
+    ingredients = listOf(
+        "Petits pois 66%",
+        "Eau",
+        "Garniture 2,8% (salade, oignon grelot)",
+        "Sucre",
+        "Sel",
+        "Arôme naturel"
+    ),
+    allergens = emptyList(),
+    additives = emptyList(),
+)
